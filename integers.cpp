23c0e378b386725e7ac9cd506b10834710b12475
rey-cyber <addoappiahf8@gmail.com>
2026-05-04 18:43:31 +0100

    NetAcad 1.2: Integer variables and basic operators

---
+++ integers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int num1 = 50;  
+    int num2 = 20;
+    
+    cout << "Num1: " << num1 << endl;
+    cout << "Num2: " << num2 << endl;
+    cout << "Sum: " << num1 + num2 << endl;
+    cout << "Difference: " << num1 - num2 << endl;
+    cout << "Product: " << num1 * num2 << endl;
+    cout << "Quotient: " << num1 / num2 << endl;
+    cout << "Remainder: " << num1 % num2 << endl;
+    
+
+    return 0;
+}
